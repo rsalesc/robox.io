cfda2e966676fd4909fbd9869c8f7fe3d12a9478
Roberto Sales <rsalesc@Robertos-MacBook-Pro.local>
2024-09-03 21:45:16 -0700

    Fix subgroup handling in tests.

--- robox/box/generators_test.py
+++ robox/box/generators_test.py
@@ -14,15 +14,22 @@ from robox.testing_utils import print_directory_tree
 def test_generator_works(pkg_from_testdata: pathlib.Path):
     generate_testcases()
     generate_outputs_for_testcases()
-    assert (package.get_build_testgroup_path('gen1') / '000.in').read_text() == '777\n'
-    assert (package.get_build_testgroup_path('gen1') / '001.in').read_text() == '123\n'
-    assert (
-        package.get_build_testgroup_path('gen1') / '002.in'
-    ).read_text() == '424242\n'
-    assert (package.get_build_testgroup_path('gen1') / '003.in').read_text() == '25\n'
 
     # Debug when fail.
     print_directory_tree(pkg_from_testdata)
+
+    assert (
+        package.get_build_testgroup_path('gen1') / '0-main-000.in'
+    ).read_text() == '777\n'
+    assert (
+        package.get_build_testgroup_path('gen1') / '1-gen-000.in'
+    ).read_text() == '123\n'
+    assert (
+        package.get_build_testgroup_path('gen1') / '1-gen-001.in'
+    ).read_text() == '424242\n'
+    assert (
+        package.get_build_testgroup_path('gen1') / '2-genScript-000.in'
+    ).read_text() == '25\n'
 
 
 @pytest.mark.test_pkg('box1')
@@ -31,9 +38,11 @@ def test_generator_cache_works(
 ):
     # Run the first time.
     generate_testcases()
-    assert (package.get_build_testgroup_path('gen1') / '001.in').read_text() == '123\n'
     assert (
-        package.get_build_testgroup_path('gen1') / '002.in'
+        package.get_build_testgroup_path('gen1') / '1-gen-000.in'
+    ).read_text() == '123\n'
+    assert (
+        package.get_build_testgroup_path('gen1') / '1-gen-001.in'
     ).read_text() == '424242\n'
 
     # Change the generator `gen1`, but keep `gen2` as is.
@@ -42,10 +51,13 @@ def test_generator_cache_works(
 
     # Run the second time.
     generate_testcases()
-    assert (package.get_build_testgroup_path('gen1') / '001.in').read_text() == '4567\n'
-    assert (
-        package.get_build_testgroup_path('gen1') / '002.in'
-    ).read_text() == '424242\n'
 
     # Debug when fail.
     print_directory_tree(pkg_from_testdata)
+
+    assert (
+        package.get_build_testgroup_path('gen1') / '1-gen-000.in'
+    ).read_text() == '4567\n'
+    assert (
+        package.get_build_testgroup_path('gen1') / '1-gen-001.in'
+    ).read_text() == '424242\n'
