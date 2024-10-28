9384a228e84e0dac4be1537c2c4d47548aa133ad
Roberto Sales <rsalesc@Robertos-MacBook-Pro.local>
2024-10-28 09:53:58 -0300

    Add _ to name field.

--- robox/box/schema.py
+++ robox/box/schema.py
@@ -15,7 +15,7 @@ Primitive = Union[str, int, float, bool]
 
 def NameField(**kwargs):
     return Field(
-        pattern=r'^[a-zA-Z0-9][a-zA-Z0-9\-]*$', min_length=3, max_length=32, **kwargs
+        pattern=r'^[a-zA-Z0-9][a-zA-Z0-9\-_]*$', min_length=3, max_length=32, **kwargs
     )
 
 
